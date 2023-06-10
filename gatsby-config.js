b5b5f36962c4f1a0b582b1009282889f95959235
Jyotiprakash <jyotiprakash@clayfin.com>
2023-06-10 22:59:57 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Demo GatsBay`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
